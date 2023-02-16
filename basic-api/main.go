7e73349b61a351024e1493ee18eac0d036de8025
tkanata <tkanata@netprotections.co.jp>
2023-02-16 15:14:10 +0900

    feat: HTMLを用いてAPIのレスポンスを確認

--- basic-api/main.go
+++ basic-api/main.go
@@ -7,17 +7,11 @@ import (
 
 func main() {
     engine := gin.Default()
-	ua := ""
-	// ミドルウェアを使用
-	engine.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
-		c.Next()
-    })
+	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-		    "message":    "hello world",
-			"User-Agent": ua,
-	    })
+		c.HTML(http.StatusOK, "index.html", gin.H{
+		    "message": "hello gin",
+		})
     })
 	engine.Run(":3000")
 }
