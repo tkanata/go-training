ac338e2eb6197f41b82dd1e7fce277c445eec2de
田中 哉太 <t.kanata@mac0469.np.co.jp>
2023-02-21 19:23:22 +0900

    feat: interactorのfuncを途中まで書いた

--- server/usecases/card/interactor.go
+++ server/usecases/card/interactor.go
@@ -1,11 +1,22 @@
 package card
 
 import (
+    "context"
     "api/server/entities"
 )
 
 type CardInteractor struct {
-    OutputPort ports.
+    OutputPort ports.RoleOutputPort
+    Repository ports.CardRepository
 }
+
+func NewUserInputPort(outputPort ports.RoleOutputPort, repository ports.CardRepository) ports.CardInputPort{
+    return &UserInteractor{
+        OutputPort: outputPort,
+        Repository: repository,
+    }
+}
+
+func (u *CardInteractor) AddCardName()
 
 
